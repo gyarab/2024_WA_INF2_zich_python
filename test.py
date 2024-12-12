20ee4840a35897092a122b9fc3841105030a5b4f
Matěj Zich <matej-krystof.zich@student.gyarab.cz>
2024-12-12 10:03:15 +0100

    yes

--- test.py
+++ test.py
@@ -13,18 +13,17 @@ def is_prime(n):
 if __name__ == "__main__":
     # Test the is_prime function
     try:
-        print(is_prime(-1))  # Output: ValueError: Input must be greater than or equal to 1
-    except ValueError as e:
-        print(e)
+         # Output: ValueError: Input must be greater than or equal to 1
+        print(is_prime(1))  # Output: False
+         # Output: TypeError: Input must be an integer
+        print(is_prime(4))  # Output: False
+        print(is_prime(3))  # Output: True
+        print(is_prime(5))  # Output: True   
+    except ValueError as ve:
+        print(ve)
+    except TypeError as te:
+        print(te)
     
-    try:
-        print(is_prime(1.5))  # Output: TypeError: Input must be an integer
-    except TypeError as e:
-        print(e)
-   
-    print(is_prime(1))  # Output: False
-    print(is_prime(4))  # Output: False
-    print(is_prime(3))  # Output: True
-    print(is_prime(5))  # Output: True
+    
 
 
