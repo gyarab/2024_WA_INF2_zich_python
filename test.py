6d847f21c0e20ef5388619422230904f27ca06fd
Matěj Zich <matej-krystof.zich@student.gyarab.cz>
2024-12-12 09:56:38 +0100

    prime final

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 def is_prime(n):
-    if n <= 1:
+    if n < 1:
+        raise ValueError("Input must be greater than or equal to 1")
+    if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
@@ -8,9 +10,15 @@ def is_prime(n):
 
 if __name__ == "__main__":
     # Test the is_prime function
+    try:
+        print(is_prime(-1))  # Output: ValueError: Input must be greater than or equal to 1
+    except ValueError as e:
+        print(e)
+    
+   
+    print(is_prime(1))  # Output: False
+    print(is_prime(4))  # Output: False
+    print(is_prime(3))  # Output: True
     print(is_prime(5))  # Output: True
-    print(is_prime(10))  # Output: False
-    print(is_prime(17))  # Output: True
-  
 
 
