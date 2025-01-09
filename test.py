4588568ec2f18647c17716e36b54f22021854f10
Matěj Zich <matej-krystof.zich@student.gyarab.cz>
2025-01-09 09:37:39 +0100

    A

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
-def primes_in_range(a, b):
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise ValueError("Both inputs must be integers")
-    if a > b:
-        a, b = b, a
-    primes = []
-    for num in range(a, b + 1):
-        if num > 1:
-            for i in range(2, int(num/2) + 1):
-                if (num % i) == 0:
-                    break
-            else:
-                primes.append(num)
-    return primes
+def split_into_threes(text):
+    if not isinstance(text, str):
+        raise ValueError("Input value must be a string.")
+    parts = []
+    for i in range(0, len(text), 3):
+        parts.append(text[i:i+3])
+    return parts
 
 if __name__ == "__main__":
-    result = primes_in_range(15, 9)
-    print(result)
+    print(split_into_threes(123))
