c7f608455cf09d9bb4513c3f7ac643fcea4c3741
Matěj Zich <matej-krystof.zich@student.gyarab.cz>
2025-01-09 08:47:56 +0100

    a

--- test.py
+++ test.py
@@ -1,29 +1,14 @@
-def is_prime(n):
-    if not isinstance(n, int):
-        raise TypeError("Input must be an integer")
-    if n < 1:
-        raise ValueError("Input must be greater than or equal to 1")
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def primes_in_range(a, b):
+    primes = []
+    for num in range(a, b + 1):
+        if num > 1:
+            for i in range(2, int(num/2) + 1):
+                if (num % i) == 0:
+                    break
+            else:
+                primes.append(num)
+    return primes
 
 if __name__ == "__main__":
-    # Test the is_prime function
-    try:
-        raise ValueError("Input must be greater than or equal to 1")
-        print(is_prime(1))  # Output: False
-         # Output: TypeError: Input must be an integer
-        print(is_prime(4))  # Output: False
-        print(is_prime(3))  # Output: True
-        print(is_prime(5))  # Output: True   
-    except ValueError as ve:
-        print(ve)
-    except TypeError as te:
-        print(te)
-    
-    
-
-
+    result = primes_in_range(1, 100)
+    print(result)
